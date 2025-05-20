06cc6c179ebbdd63a4cfeb7ac3f2087344206215
Kim, seungwon <ksw12038@gmail.com>
2025-05-21 04:49:04 +0900

    three

--- src/gittest/Main.java
+++ src/gittest/Main.java
@@ -3,6 +3,6 @@ public class Main {
     System.out.println("202314149 김승원");
     System.out.println(Hello.sum(10));
     System.out.println(Hello.sum(100));
-
+    System.out.println(Hello.sum(1000));
   }
 }
